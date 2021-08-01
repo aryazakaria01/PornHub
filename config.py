56f76b648f7cd26c30b9040a66252a3f738e736a
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-01 16:28:14 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
+REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/aryazakaria01/PornHub")
+OWNER = os.environ.get("OWNER", "")
+BOT_NAME = os.environ.get("BOT_NAME", "")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "DolaSupport")
