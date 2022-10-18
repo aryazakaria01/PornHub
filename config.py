bfbe074be3f8a8caccc0a138db787adc5575ec31
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-10-18 11:35:58 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-TOKEN = os.environ.get("TOKEN", "")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
-REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/aryazakaria01/PornHub")
-OWNER = os.environ.get("OWNER", "")
-BOT_NAME = os.environ.get("BOT_NAME", "")
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "SakuraEmpireTeam")
