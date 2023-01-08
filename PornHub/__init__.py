ede3eae0046b855da51b67c5e01c8fea8a8360db
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2023-01-08 15:46:39 +0700

    Create __init__.py

---
+++ PornHub/__init__.py
@@ -0,0 +1,10 @@
+from subprocess import run
+
+
+__version__ = "2.0.1b4"
+__version_code__ = (
+    run(["git", "rev-list", "--count", "HEAD"], capture_output=True)
+    .stdout.decode()
+    .strip()
+    or "0"
+)
