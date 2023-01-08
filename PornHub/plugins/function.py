d156ebe70f5a0d51d0bcd1001e1f4deb51159e8f
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2023-01-08 15:52:46 +0700

    Create function.py

---
+++ PornHub/plugins/function.py
@@ -0,0 +1,38 @@
+import asyncio
+import threading
+from pyrogram.errors import MessageNotModified, FloodWait
+
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def edit_msg(client, message, to_edit):
+    try:
+        client.loop.create_task(message.edit(to_edit))
+    except FloodWait as e:
+        client.loop.create_task(asyncio.sleep(e.value))
+    except MessageNotModified:
+        pass
+    except TypeError:
+        pass
+
+
+def download_progress_hook(d, message, client):
+    if d['status'] == 'downloading':
+        current = d.get("_downloaded_bytes_str") or humanbytes(int(d.get("downloaded_bytes", 1)))
+        total = d.get("_total_bytes_str") or d.get("_total_bytes_estimate_str")
+        file_name = d.get("filename")
+        eta = d.get('_eta_str', "N/A")
+        percent = d.get("_percent_str", "N/A")
+        speed = d.get("_speed_str", "N/A")
+        to_edit = f"📥 <b>Downloading!</b>\n\n<b>Name :</b> <code>{file_name}</code>\n<b>Size :</b> <code>{total}</code>\n<b>Speed :</b> <code>{speed}</code>\n<b>ETA :</b> <code>{eta}</code>\n\n<b>Percentage: </b> <code>{current}</code> from <code>{total} (__{percent}__)</code>"
+        threading.Thread(target=edit_msg, args=(client, message, to_edit)).start()
