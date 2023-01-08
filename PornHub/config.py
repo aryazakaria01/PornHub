8da147ed0676150825faff7abe71ae61a3b28fd4
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2023-01-08 15:48:47 +0700

    Create config.py

---
+++ PornHub/config.py
@@ -0,0 +1,17 @@
+from typing import List
+
+API_ID: int = insert_your_own_api_id_here
+API_HASH: str = "insert_your_own_api_hash_here"
+TOKEN: str = "insert_your_bot_token_here"
+
+log_chat: int = insert_log_chat_id_here
+sub_chat: str = "insert_channel_username_here"
+sudoers: List[int] = [1757169682, 859229457]
+prefixs: List[str] = ["/", "!", ".", "$", "-"]
+
+# notes
+
+# 1. api_id & api_hash get from my.telegram.org
+# 2. token fill with your bot_token get from @BotFather
+# 3. log_chat fill with the chat id of a group that you should create for the bot logger
+# 4. sub_chat fill with the channel username but don't include the '@' symbol
