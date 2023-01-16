fe9e3646c84c3b4c78d48560dc68b7394ade5bfb
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2023-01-16 12:44:48 +0700

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -4,9 +4,9 @@ API_ID: int = insert_your_own_api_id_here
 API_HASH: str = "insert_your_own_api_hash_here"
 TOKEN: str = "insert_your_bot_token_here"
 
-log_chat: int = insert_log_chat_id_here
-sub_chat: str = "insert_channel_username_here"
-sudoers: List[int] = [1757169682, 859229457]
+log_chat: int = -1001662591986
+sub_chat: str = "SakuraEmpireTeam"
+sudoers: List[int] = [2137482758, 1249591948]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
 
 # notes
