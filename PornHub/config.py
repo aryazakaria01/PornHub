8a5220b325c2e9b50f01b4b82a48b78a30402355
elCahpo <123294919+elCahpo@users.noreply.github.com>
2023-11-18 00:52:49 +0700

    config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -4,7 +4,7 @@ API_ID: int = 8477635
 API_HASH: str = "7f6c3de9e4f760752e02da32c401f24d"
 TOKEN: str = "6869078764:AAHi4EuUBSf0vI5j1xi5ivrzalkSJHNLWSg"
 
-log_chat: int = -1001662591986
+log_chat: int = -1002110637937
 sub_chat: str = "PremiumVPS"
 sudoers: List[int] = [2137482758, 1249591948]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
