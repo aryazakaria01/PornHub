5666cf470d4fb23185917164184c656fa823277b
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-07-26 15:30:48 +0700

    better logs

--- PornHub/__main__.py
+++ PornHub/__main__.py
@@ -6,15 +6,23 @@ from .bot import PornHub
 from pyrogram import idle
 from asyncio import get_event_loop_policy
 
+LOG_FILE_NAME = "PhLogs.txt"
+timezone = pytz.timezone('Asia/Jakarta')
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(name)s.%(funcName)s | %(levelname)s | %(message)s",
-    datefmt="[%X]",
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt=datetime.now(timezone).strftime("%d - %b - %y | %H:%M:%S"),
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
 )
-logging.getLogger("pyrogram.syncer").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
-
+logging.getLogger("pyrogram.crypto.aes").setLevel(logging.INFO)
+logging.getLogger("pyrogram.session.session").setLevel(logging.INFO)
+logging.getLogger("pyrogram.connection.connection").setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
 
